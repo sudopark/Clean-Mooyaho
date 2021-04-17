eece33b75f1e8f0bc6fa998bfaa82d0d1b7157f0
sudo.park <sudo.park@kakao.com>
2021-04-18 05:37:32 +0900

    Domain, DataStore framework 추가 및  development target 11 로 세팅

---
+++ DataStore/DataStore/DataStore.h
@@ -0,0 +1,19 @@
+//
+//  DataStore.h
+//  DataStore
+//
+//  Created by ParkHyunsoo on 2021/04/18.
+//  Copyright © 2021 ParkHyunsoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStore.
+FOUNDATION_EXPORT double DataStoreVersionNumber;
+
+//! Project version string for DataStore.
+FOUNDATION_EXPORT const unsigned char DataStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStore/PublicHeader.h>
+
+
---
+++ Domain/Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by ParkHyunsoo on 2021/04/18.
+//  Copyright © 2021 ParkHyunsoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
