c36bfaf5e7ba0479abd62e850db37a543adc4c94
sudo.park <sudo.park@kakao.com>
2021-04-21 14:34:32 +0900

    setup firebase

--- BreadRoadApp/BreadRoadApp/AppDelegate.swift
+++ BreadRoadApp/BreadRoadApp/AppDelegate.swift
@@ -11,10 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    private let firebaseService: FirebaseServiceInterface = FirebaseService()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
+        self.firebaseService.setup()
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
 
         let viewController = ViewController()
