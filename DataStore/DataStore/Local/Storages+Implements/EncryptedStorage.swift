938728af7f10947bd12cfee334794af81587afc6
sudo.park <sudo.park@kakao.com>
2021-04-24 14:48:54 +0900

    DataLayer 필요 Model Protocol 선언

---
+++ DataStore/DataStore/Local/Storages+Implements/EncryptedStorage.swift
@@ -0,0 +1,12 @@
+//
+//  EncryptedStorage.swift
+//  DataStore
+//
+//  Created by ParkHyunsoo on 2021/04/24.
+//  Copyright © 2021 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol EncryptedStorage { }
