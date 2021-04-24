c3e8e65bb03c902c74d3e25fef9015d377a30a27
sudo.park <sudo.park@kakao.com>
2021-04-24 14:48:54 +0900

    DataLayer 필요 Model Protocol 선언

---
+++ DataStore/DataStore/Remote/Remote.swift
@@ -0,0 +1,12 @@
+//
+//  Reomte.swift
+//  DataStore
+//
+//  Created by ParkHyunsoo on 2021/04/24.
+//  Copyright © 2021 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
+
+
+public protocol Remote { }
