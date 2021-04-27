7162f0c854ad97fbbaf7c6fa23585d30a60e2df1
sudo.park <sudo.park@kakao.com>
2021-04-28 02:04:01 +0900

    add CommonPresenting framework

---
+++ Presentations/CommonPresenting/CommonPresenting/CommonPresenting.h
@@ -0,0 +1,18 @@
+//
+//  CommonPresenting.h
+//  CommonPresenting
+//
+//  Created by ParkHyunsoo on 2021/04/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonPresenting.
+FOUNDATION_EXPORT double CommonPresentingVersionNumber;
+
+//! Project version string for CommonPresenting.
+FOUNDATION_EXPORT const unsigned char CommonPresentingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonPresenting/PublicHeader.h>
+
+
