30fb32d7846a88a48d65401978f4f4bf85b83cb8
sudo.park <sudo.park@kakao.com>
2021-05-14 03:40:06 +0900

    무야호 전파 + 수신 관련 usecase 인터페이스 정의

---
+++ Domain/Domain/Usecases/Hooray/HoorayReceiveUsecase.swift
@@ -0,0 +1,56 @@
+//
+//  HoorayReceiveUsecase.swift
+//  Domain
+//
+//  Created by sudo.park on 2021/05/14.
+//  Copyright © 2021 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+
+// MARK: - HoorayReceiverUsecase
+
+public protocol HoorayReceiverUsecase { }
+
+
+// MARK: - HoorayReceiverUsecaseImple
+
+public final class HoorayReceiverUsecaseImple: HoorayReceiverUsecase {
+    
+    private let messageService: MessageService
+    
+    public init(messageService: MessageService) {
+        self.messageService = messageService
+    }
+}
+
+
+extension HoorayReceiverUsecaseImple {
+    
+    enum AliveHoorayDetectionPolicy {
+        
+        static let validDistance: Meters = 0
+        static let alivableTimeInterval: TimeInterval = 0
+    }
+    
+    public func loadNearbyRecentHoorays(at location: Coordinate) -> Maybe<[Hooray]> {
+        // TODO: 근처에 유효한 후레이 조회 + ack 처리
+        return .empty()
+    }
+    
+    public func ackReceivedHooray(_ hoorayID: String) -> Maybe<Void> {
+        // TODO: 후레이 수신확인
+        return .empty()
+    }
+}
+
+extension HoorayReceiverUsecaseImple {
+    
+    public var newReceivedHooray: Observable<Hooray> {
+        // TODO: 실시간 받은 무야호 필터링해서 전파
+        .empty()
+    }
+}
