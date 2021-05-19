bb76597428fd08315eda8911b398806f74c89b18
sudo.park <sudo.park@kakao.com>
2021-05-20 01:20:06 +0900

    Messaging remote api 구현

---
+++ FirebaseService/FirebaseService/RemoteImple+FirebaseService/MessagingRemote+Firebase.swift
@@ -0,0 +1,114 @@
+//
+//  MessagingRemote+Firebase.swift
+//  FirebaseService
+//
+//  Created by sudo.park on 2021/05/19.
+//
+
+import Foundation
+
+import RxSwift
+
+import Domain
+import DataStore
+
+
+extension FirebaseServiceImple: MessagingRemote { }
+
+
+extension FirebaseServiceImple {
+    
+    func requestLoadOnlineUserDevices(_ userID: String) -> Maybe<[UserDevices]> {
+        typealias Key = UserDeviceMappingKey
+        let colllectionRef = self.fireStoreDB.collection(.userDevice)
+        let query = colllectionRef
+            .whereField(FieldPath.documentID(), isEqualTo: userID)
+            .whereField(Key.isOnline.rawValue, isEqualTo: true)
+        return self.load(query: query)
+    }
+    
+    public func requestSendForground(message: Messsage, to userID: String) -> Maybe<Void> {
+        
+        guard let payload = (message as? MessagePayloadConvertable)?.dataPayload() else {
+            return .empty()
+        }
+        
+        let loadOnlineDevices = self.requestLoadOnlineUserDevices(userID)
+        let preparePayloads: ([UserDevices]) -> [String: Any] = { devices in
+             return [
+                "registration_ids": devices.map{ $0.token },
+                "data": payload
+            ]
+        }
+        let sendMessages: ([String: Any]) -> Maybe<Void> = { [weak self] payload in
+            return self?.requestSendForgroundMessage(payload) ?? .empty()
+        }
+        
+        return loadOnlineDevices
+            .map(preparePayloads)
+            .flatMap(sendMessages)
+    }
+    
+    func batchSendForgroundMessages(_ message: Messsage, toUsers userIDs: [String]) {
+        typealias Key = UserDeviceMappingKey
+        
+        guard let payload = (message as? MessagePayloadConvertable)?.dataPayload() else { return }
+        
+        let collectionRef = self.fireStoreDB.collection(.userDevice)
+        let slices = userIDs.slice(by: 10)
+        let queries = slices.map { slice in
+            return collectionRef.whereField(FieldPath.documentID(), in: slices)
+                .whereField(Key.isOnline.rawValue, isEqualTo: true)
+        }
+        let onlineDevices: Observable<[UserDevices]> = self.loadAll(queries: queries)
+        let asPayload: ([UserDevices]) -> [[String: Any]] = { devices in
+            let sliceDevices = devices.slice(by: maxFcmSendrequestCount)
+            return sliceDevices.map {
+                [
+                    "registration_ids": $0.map{ $0.token },
+                    "data": payload
+                ]
+            }
+        }
+        
+        onlineDevices.map(asPayload)
+            .subscribe(onNext: { [weak self] payloads in
+                guard let self = self else { return }
+                self.disposeBag.insert(
+                    payloads.map{ self.requestSendForgroundMessage($0).subscribe() }
+                )
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    private func requestSendForgroundMessage(_ payload: [String: Any]) -> Maybe<Void> {
+        guard self.serverKey.isNotEmpty else { return .empty() }
+        let endpoint = FcmAPIEndPoint(serverKey: self.serverKey)
+        return self.httpAPI.requestResult(endpoint, parameters: payload)
+    }
+}
+
+
+
+protocol MessagePayloadConvertable {
+    
+    func dataPayload() -> [String: Any]
+    
+    func notificationPayload(for device: UserDevices) -> [String: Any]
+}
+
+extension MessagePayloadConvertable {
+    func notificationPayload(for device: UserDevices) -> [String: Any] { [:] }
+}
+
+
+extension HoorayAckMessage: MessagePayloadConvertable {
+    
+    func dataPayload() -> [String : Any] {
+        return [
+            "m_type": "hooray_ack",
+            "ack_uid": self.ackUserID,
+            "hid": self.hoorayID
+        ]
+    }
+}
