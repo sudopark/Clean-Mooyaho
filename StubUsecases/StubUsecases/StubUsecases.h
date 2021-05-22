58534ebdbb23a0e6f7b70203a3ecb1ce6853c0c0
sudo.park <sudo.park@kakao.com>
2021-05-23 00:22:04 +0900

    프로젝트 최소지원버전 12로 샹향 및 StubUsecase framework 추가

---
+++ StubUsecases/StubUsecases/StubUsecases.h
@@ -0,0 +1,18 @@
+//
+//  StubUsecases.h
+//  StubUsecases
+//
+//  Created by sudo.park on 2021/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StubUsecases.
+FOUNDATION_EXPORT double StubUsecasesVersionNumber;
+
+//! Project version string for StubUsecases.
+FOUNDATION_EXPORT const unsigned char StubUsecasesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StubUsecases/PublicHeader.h>
+
+
