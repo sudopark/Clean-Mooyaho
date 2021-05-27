a64e48503554aa85b437f75ea17650925e4eb0d4
sudo.park <sudo.park@kakao.com>
2021-05-28 03:23:44 +0900

    PlaceScenes framework 추가

---
+++ Presentations/PlaceScenes/PlaceScenes/PlaceScenes.h
@@ -0,0 +1,18 @@
+//
+//  PlaceScenes.h
+//  PlaceScenes
+//
+//  Created by sudo.park on 2021/05/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaceScenes.
+FOUNDATION_EXPORT double PlaceScenesVersionNumber;
+
+//! Project version string for PlaceScenes.
+FOUNDATION_EXPORT const unsigned char PlaceScenesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaceScenes/PublicHeader.h>
+
+
