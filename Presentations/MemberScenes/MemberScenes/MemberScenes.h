0472ed9e99aa333645f0e10afcd965f37ea93196
sudo.park <sudo.park@kakao.com>
2021-05-29 05:10:32 +0900

    MemberScenes framework 추가

---
+++ Presentations/MemberScenes/MemberScenes/MemberScenes.h
@@ -0,0 +1,18 @@
+//
+//  MemberScenes.h
+//  MemberScenes
+//
+//  Created by sudo.park on 2021/05/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MemberScenes.
+FOUNDATION_EXPORT double MemberScenesVersionNumber;
+
+//! Project version string for MemberScenes.
+FOUNDATION_EXPORT const unsigned char MemberScenesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MemberScenes/PublicHeader.h>
+
+
