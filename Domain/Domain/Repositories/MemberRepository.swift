7a512c8d9f49679f4ad0b04f436ce45d1dbb8107
sudo.park <sudo.park@kakao.com>
2021-06-02 00:27:58 +0900

    멤버 프로필 업데이트 도메인로직 작성

--- Domain/Domain/Repositories/MemberRepository.swift
+++ Domain/Domain/Repositories/MemberRepository.swift
@@ -11,9 +11,20 @@ import Foundation
 import RxSwift
 
 
+public enum MemberProfileUploadStatus {
+    case uploading(_ percent: Float)
+    case completed(_ source: ImageSource?)
+}
+
 public protocol MemberRepository {
     
     func requestUpdateUserPresence(_ userID: String, isOnline: Bool) -> Maybe<Void>
     
     func requestLoadMembership(for memberID: String) -> Maybe<MemberShip>
+    
+    func requestUploadMemberProfileImage(_ memberID: String, source: MemberProfileImageSources) -> Observable<MemberProfileUploadStatus>
+    
+    func requestUpdateMemberProfileFields(_ memberID: String,
+                                          fields: [MemberUpdateField],
+                                          imageSource: ImageSource?) -> Maybe<Void>
 }
--- Domain/Domain/Usecases/Auth+Member/MemberUsecase.swift
+++ Domain/Domain/Usecases/Auth+Member/MemberUsecase.swift
@@ -11,6 +11,24 @@ import Foundation
 import RxSwift
 
 
+public enum UpdateMemberProfileStatus: Equatable {
+    
+    case pending
+    case updating(_ percent: Float)
+    case finishedWithImageUploadFail(_ error: Error)
+    case finished
+    
+    public static func == (lhs: UpdateMemberProfileStatus, rhs: UpdateMemberProfileStatus) -> Bool {
+        switch (lhs, rhs) {
+        case (.pending, .pending): return true
+        case let (.updating(p1), .updating(p2)): return p1 == p2
+        case (.finishedWithImageUploadFail, .finishedWithImageUploadFail): return true
+        case (.finished, .finished): return true
+        default: return false
+        }
+    }
+}
+
 // MARK: - MemberUsecase
 
 public protocol MemberUsecase {
@@ -18,6 +36,10 @@ public protocol MemberUsecase {
     func fetchCurrentMember() -> Member?
     
     func updateUserIsOnline(_ userID: String, isOnline: Bool)
+    
+    func updateCurrent(memberID: String,
+                       updateFields: [MemberUpdateField],
+                       with profile: MemberProfileImageSources?) -> Observable<UpdateMemberProfileStatus>
     
     func loadCurrentMembership() -> Maybe<MemberShip>
     
@@ -54,6 +76,55 @@ extension MemberUsecaseImple {
             .disposed(by: self.disposeBag)
     }
     
+    private func finishUpdateMember(_ memberID: String,
+                                    fields: [MemberUpdateField],
+                                    imageSource: ImageSource? = nil) -> Observable<UpdateMemberProfileStatus> {
+        return self.memberRepository
+            .requestUpdateMemberProfileFields(memberID, fields: fields, imageSource: imageSource)
+            .asObservable()
+            .map{ _ in UpdateMemberProfileStatus.finished }
+    }
+    
+    public func updateCurrent(memberID: String,
+                              updateFields: [MemberUpdateField],
+                              with profile: MemberProfileImageSources?) -> Observable<UpdateMemberProfileStatus> {
+        
+        let isRequestParametersEmpty = updateFields.isEmpty && profile == nil
+        guard isRequestParametersEmpty == false else {
+            return .error(ApplicationErrors.invalid)
+        }
+        
+        // check valid params
+        guard let profile = profile else {
+            return self.finishUpdateMember(memberID, fields: updateFields)
+        }
+    
+        let uploadImage = self.memberRepository.requestUploadMemberProfileImage(memberID, source: profile)
+        
+        let thenUpdateFieldOrBypassEvent: (MemberProfileUploadStatus) -> Observable<UpdateMemberProfileStatus>
+        thenUpdateFieldOrBypassEvent = { [weak self] uploadStatus in
+            guard let self = self else { return .empty() }
+            switch uploadStatus {
+            case let .uploading(percent):
+                return .just(.updating(percent))
+            case let .completed(source):
+                return self.finishUpdateMember(memberID, fields: updateFields, imageSource: source)
+            }
+        }
+        
+        let errorthenJustUpdateFields: (Error) -> Observable<UpdateMemberProfileStatus> = { [weak self] error in
+            guard let self = self else { return .empty() }
+            return self.memberRepository.requestUpdateMemberProfileFields(memberID, fields: updateFields, imageSource: nil)
+                .asObservable()
+                .map{ _ in UpdateMemberProfileStatus.finishedWithImageUploadFail(error) }
+        }
+        
+        return uploadImage
+            .flatMap(thenUpdateFieldOrBypassEvent)
+            .startWith(.pending)
+            .catch(errorthenJustUpdateFields)
+    }
+    
     
 //    public func loadNearbyUsers(at location: Coordinate) -> Maybe<[UserPresence]> {
 //        return self.memberRepository
@@ -77,5 +148,19 @@ extension MemberUsecaseImple {
     public var currentMember: Observable<Member?> {
         return self.sharedDataStoreService
             .observe(SharedDataKeys.currentMember.rawValue)
+    }
+}
+
+
+private extension MemberProfileUploadStatus {
+    
+    var uploadingPercent: Float? {
+        guard case let .uploading(percent) = self else { return nil }
+        return percent
+    }
+    
+    var completedSource: ImageSource? {
+        guard case let .completed(source) = self else { return nil }
+        return source
     }
 }
