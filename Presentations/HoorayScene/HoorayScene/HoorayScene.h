6ddfa12f44db9165a205fb75d3a1c0158c69bf4f
sudo.park <sudo.park@kakao.com>
2021-06-04 06:29:50 +0900

    HoorayScene 추가

---
+++ Presentations/HoorayScene/HoorayScene/HoorayScene.h
@@ -0,0 +1,18 @@
+//
+//  HoorayScene.h
+//  HoorayScene
+//
+//  Created by sudo.park on 2021/06/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HoorayScene.
+FOUNDATION_EXPORT double HooraySceneVersionNumber;
+
+//! Project version string for HoorayScene.
+FOUNDATION_EXPORT const unsigned char HooraySceneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HoorayScene/PublicHeader.h>
+
+
