d835907de3c343dc2ae0e6802dde483a9fc0cea6
sudo.park <sudo.park@kakao.com>
2021-06-09 03:04:01 +0900

    장소 서제스트 뷰모델 구현

--- Presentations/HoorayScene/HoorayScene/SelectHoorayPlace/SelectHoorayPlaceViewModel.swift
+++ Presentations/HoorayScene/HoorayScene/SelectHoorayPlace/SelectHoorayPlaceViewModel.swift
@@ -17,11 +17,34 @@ import CommonPresenting
 
 // MARK: - SelectHoorayPlaceViewModel
 
+public struct SuggestPlaceCellViewModel {
+    
+    public let placeID: String
+    public let title: String
+    public let position: Coordinate
+    public var distance: String = ""
+    public var isSelected: Bool = false
+    
+    public init(snippet: PlaceSnippet) {
+        self.placeID = snippet.placeID
+        self.title = snippet.title
+        self.position = .init(latt: snippet.latt, long: snippet.long)
+    }
+    
+    func distanceCalculated(from userPosition: Coordinate) -> Self {
+        var sender = self
+        let distanceMeters = sender.position.distance(from: userPosition)
+        sender.distance = "\(distanceMeters)m"
+        return sender
+    }
+}
+
 public protocol SelectHoorayPlaceViewModel: AnyObject {
 
     // interactor
     
     // presenter
+    var currentUserLocation: Observable<LastLocation> { get }
     var goNextStepWithForm: Observable<NewHoorayForm> { get }
 }
 
@@ -31,13 +54,21 @@ public protocol SelectHoorayPlaceViewModel: AnyObject {
 public final class SelectHoorayPlaceViewModelImple: SelectHoorayPlaceViewModel {
     
     private let form: NewHoorayForm
+    private let userLocationUsecase: UserLocationUsecase
+    private let suggestPlaceUsecase: SuggestPlaceUsecase
     private let router: SelectHoorayPlaceRouting
     
     public init(form: NewHoorayForm,
+                userLocationUsecase: UserLocationUsecase,
+                suggestPlaceUsecase: SuggestPlaceUsecase,
                 router: SelectHoorayPlaceRouting) {
         
         self.form = form
+        self.userLocationUsecase = userLocationUsecase
+        self.suggestPlaceUsecase = suggestPlaceUsecase
         self.router = router
+        
+        self.internalBinding()
     }
     
     deinit {
@@ -45,6 +76,9 @@ public final class SelectHoorayPlaceViewModelImple: SelectHoorayPlaceViewModel {
     }
     
     fileprivate final class Subjects {
+        let currentUserLocation = BehaviorRelay<LastLocation?>(value: nil)
+        let selectedPlaceID = BehaviorRelay<String?>(value: nil)
+        let cellViewModels = BehaviorRelay<[SuggestPlaceCellViewModel]>(value: [])
         let continueNext = PublishSubject<NewHoorayForm>()
     }
     
@@ -57,6 +91,52 @@ public final class SelectHoorayPlaceViewModelImple: SelectHoorayPlaceViewModel {
 
 extension SelectHoorayPlaceViewModelImple {
     
+    public func suggestPlace(by title: String) {
+        guard let lastLocation = self.subjects.currentUserLocation.value else { return }
+        self.requestSuggestPlace(.some(title), in: lastLocation)
+    }
+    
+    public func refreshUserLocation() {
+        self.fetchUserLocationAndUpdateList()
+    }
+    
+    public func toggleUpdateSelected(_ placeID: String) {
+        let previousSelectedID = self.subjects.selectedPlaceID.value
+        let shouldSelect = previousSelectedID != placeID
+        if shouldSelect {
+            self.subjects.selectedPlaceID.accept(placeID)
+        } else {
+            self.subjects.selectedPlaceID.accept(nil)
+        }
+    }
+    
+    public func skipPlaceInput() {
+        
+        let confirmed: () -> Void = { [weak self] in
+            logger.todoImplement("should move to place select scene")
+        }
+        guard let form = AlertBuilder(base: .init())
+                .message("[TBD] message")
+                .confirmed(confirmed)
+                .build() else { return }
+        
+        self.router.alertForConfirm(form)
+    }
+    
+    public func registerNewPlace() {
+
+        self.router.presentNewPlaceRegisterScene()
+    }
+    
+    private var memberID: String {
+        return self.form.publisherID
+    }
+    
+    private func requestSuggestPlace(_ query: SuggestPlaceQuery, in lastLocation: LastLocation) {
+        let userLocation = UserLocation(userID: self.memberID, lastLocation: lastLocation)
+        self.suggestPlaceUsecase.startSuggestPlace(for: query, in: userLocation)
+    }
+    
 }
 
 
@@ -64,7 +144,101 @@ extension SelectHoorayPlaceViewModelImple {
 
 extension SelectHoorayPlaceViewModelImple {
     
+    public var currentUserLocation: Observable<LastLocation> {
+        return self.subjects.currentUserLocation.compactMap{ $0 }
+    }
+    
+    public var cellViewModels: Observable<[SuggestPlaceCellViewModel]> {
+        return self.subjects.cellViewModels.asObservable()
+    }
+    
+    public var selectedPlaceID: Observable<String> {
+        return self.subjects.selectedPlaceID.compactMap{ $0 }
+    }
+    
+    public var isFinishInputEnabled: Observable<Bool> {
+        return self.subjects.selectedPlaceID
+            .map{ $0 != nil }
+            .distinctUntilChanged()
+    }
+    
     public var goNextStepWithForm: Observable<NewHoorayForm> {
         return self.subjects.continueNext.asObservable()
+    }
+}
+
+
+// MARK: - internal bind
+
+extension SelectHoorayPlaceViewModelImple {
+    
+    private typealias CVM = SuggestPlaceCellViewModel
+    
+    private var suggestedCellViewModels: Observable<[CVM]> {
+        
+        let cellViewModels = self.suggestPlaceUsecase.placeSuggestResult
+            .map{ $0?.places.asCellViewModels() ?? [] }
+        
+        let applySelectedInfo: ([CVM], String?) -> [CVM] = { cellViewModels, selectedID in
+            return cellViewModels.toggleSelected(selectedID)
+        }
+        
+        let applyDistance: ([CVM], LastLocation?) -> [CVM] = { cellViewModels, location in
+            guard let location = location else { return cellViewModels }
+            let coordinate = Coordinate(latt: location.lattitude, long: location.longitude)
+            return cellViewModels.map{ $0.distanceCalculated(from: coordinate) }
+        }
+
+        return Observable
+            .combineLatest(cellViewModels, self.subjects.selectedPlaceID,
+                           resultSelector: applySelectedInfo)
+            .withLatestFrom(self.subjects.currentUserLocation,
+                            resultSelector: applyDistance)
+    }
+    
+    private func internalBinding() {
+        
+        self.suggestedCellViewModels
+            .subscribe(onNext: { [weak self] cellViewModels in
+                self?.subjects.cellViewModels.accept(cellViewModels)
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.fetchUserLocationAndUpdateList()
+    }
+    
+    private func fetchUserLocationAndUpdateList() {
+        self.userLocationUsecase.fetchUserLocation()
+            .subscribe(onSuccess: { [weak self] location in
+                self?.subjects.currentUserLocation.accept(location)
+                self?.requestSuggestPlace(.empty, in: location)
+            })
+            .disposed(by: self.disposeBag)
+    }
+}
+
+
+private extension LastLocation {
+    
+    func asUserLocation(_ memberID: String) -> UserLocation {
+        return .init(userID: memberID, lastLocation: self)
+    }
+}
+
+private extension Array where Element == PlaceSnippet {
+    
+    func asCellViewModels() -> [SuggestPlaceCellViewModel] {
+        return self.map(SuggestPlaceCellViewModel.init(snippet:))
+    }
+}
+
+private extension Array where Element == SuggestPlaceCellViewModel {
+    
+    func toggleSelected(_ placeID: String?) -> Array {
+        return self.map { cellViewModel in
+            var cellViewModel = cellViewModel
+            cellViewModel.isSelected = cellViewModel.placeID == placeID
+            return cellViewModel
+        }
     }
 }
---
+++ Presentations/HoorayScene/HooraySceneTests/SelectHoorayPlaceViewModelTests.swift
@@ -0,0 +1,245 @@
+//
+//  SelectHoorayPlaceViewModelTests.swift
+//  HooraySceneTests
+//
+//  Created by sudo.park on 2021/06/09.
+//
+
+import XCTest
+
+import RxSwift
+
+import Domain
+import CommonPresenting
+import UnitTestHelpKit
+import StubUsecases
+
+@testable import HoorayScene
+
+
+class SelectHoorayPlaceViewModelTests: BaseTestCase, WaitObservableEvents {
+    
+    var disposeBag: DisposeBag!
+    var stubLocationUsecase: StubUserLocationUsecase!
+    var stubSuggestUsecase: StubSuggestPlaceUsecase!
+    var spyRouter: SpyRouter!
+    var viewModel: SelectHoorayPlaceViewModelImple!
+    
+    override func setUpWithError() throws {
+        self.disposeBag = .init()
+        self.stubLocationUsecase = .init()
+        self.stubSuggestUsecase = .init()
+        self.spyRouter = .init()
+        self.initViewModel()
+    }
+    
+    override func tearDownWithError() throws {
+        self.disposeBag = nil
+        self.stubLocationUsecase = nil
+        self.stubSuggestUsecase = nil
+        self.spyRouter = nil
+        self.viewModel = nil
+    }
+    
+    private func initViewModel() {
+        self.viewModel = .init(form: .init(publisherID: "some"),
+                               userLocationUsecase: self.stubLocationUsecase,
+                               suggestPlaceUsecase: self.stubSuggestUsecase,
+                               router: self.spyRouter)
+    }
+}
+
+extension SelectHoorayPlaceViewModelTests {
+    
+    private func stubDefaultList(_ range: Range<Int> = (0..<1)) {
+        self.stubLocationUsecase.register(key: "fetchUserLocation") {
+            return Maybe<LastLocation>.just(.init(lattitude: 0, longitude: 0, timeStamp: 0))
+        }
+        self.stubSuggestUsecase.register(type: SuggestPlaceResult.self, key: "startSuggestPlace:") {
+            let dummies = range.map{ PlaceSnippet.dummy($0) }
+            let result = SuggestPlaceResult(default: dummies)
+            return result
+        }
+    }
+    
+    // 최초에 현재 유저위치 불러옴
+    func testViewModel_firstLoadCurrentUserLocation() {
+        // given
+        let expect = expectation(description: "최초에 현재 유저위치 표시")
+        self.stubDefaultList()
+        
+        // when
+        self.initViewModel()
+        let location = self.waitFirstElement(expect, for: self.viewModel.currentUserLocation) { }
+        
+        // then
+        XCTAssertNotNil(location)
+    }
+    
+    // 현재 유저위치 불러와서 -> 리스트 구성
+    func testViewModel_firstSuggestPlacesNearbyUser() {
+        // given
+        let expect = expectation(description: "최초에 현재 유저위치 기준으로 장소 서제스트")
+        self.stubDefaultList()
+        
+        // when
+        self.initViewModel()
+        let places = self.waitFirstElement(expect, for: self.viewModel.cellViewModels) { }
+        
+        // then
+        XCTAssertEqual(places?.count, 1)
+    }
+    
+    // 장소목록 키워드 필터링
+    func testViewModel_filterPlacesByKeyword() {
+        // given
+        let expect = expectation(description: "장소 서제스트목록 키워드 필터링")
+        expect.expectedFulfillmentCount = 2
+        
+        self.stubDefaultList()
+        self.stubSuggestUsecase.register(type: SuggestPlaceResult.self, key: "startSuggestPlace:some") {
+            let result = SuggestPlaceResult(default: [PlaceSnippet.dummy(1)])
+            return result
+        }
+        
+        // when
+        self.initViewModel()
+        let placeLists = self.waitElements(expect, for: self.viewModel.cellViewModels) {
+            self.viewModel.suggestPlace(by: "some")
+        }
+        
+        // then
+        let (page1, page2) = (placeLists.first, placeLists.last)
+        XCTAssertEqual(page1?.count, 1)
+        XCTAssertEqual(page1?.first?.placeID, "uid:0")
+        XCTAssertEqual(page2?.count, 1)
+        XCTAssertEqual(page2?.first?.placeID, "uid:1")
+    }
+    
+    // 항목 선택시에 체크토글
+    func testViewModel_whenSelectPlace_updateList() {
+        // given
+        let expect = expectation(description: "장소 선택시에 선택플래그 토글")
+        expect.expectedFulfillmentCount = 4
+        self.stubDefaultList(0..<10)
+        
+        // when
+        self.initViewModel()
+        let selectedIDSource = self.viewModel.cellViewModels.map{ $0.filter{ $0.isSelected } }
+        let selectedIDLists = self.waitElements(expect, for: selectedIDSource) {
+            self.viewModel.toggleUpdateSelected("uid:2")
+            self.viewModel.toggleUpdateSelected("uid:4")
+            self.viewModel.toggleUpdateSelected("uid:4")
+        }
+        
+        // then
+        let selectedIDs = selectedIDLists.map{ $0.first?.placeID }
+        XCTAssertEqual(selectedIDs, [nil, "uid:2", "uid:4", nil])
+    }
+    
+    // 항목 선택시에 아이콘
+    func testViewModel_whenSelectPlace_updateSelectPlaceID() {
+        // given
+        let expect = expectation(description: "장소 선택시에 선택플래그 토글")
+        self.stubDefaultList(0..<10)
+        
+        // when
+        self.initViewModel()
+        let selectedID = self.waitFirstElement(expect, for: self.viewModel.selectedPlaceID) {
+            self.viewModel.toggleUpdateSelected("uid:3")
+        }
+        
+        // then
+        XCTAssertEqual(selectedID, "uid:3")
+    }
+    
+    // 항목 선택시에 입력종료 버튼 활성화
+    func testViewModel_updateFinishInputEnable_bySelectedInfo() {
+        // given
+        let expect = expectation(description: "선택된 장소정보 여부에 따라 입력 종료버튼 활성화")
+        expect.expectedFulfillmentCount = 3
+        self.stubDefaultList(0..<10)
+
+        // when
+        self.initViewModel()
+        let isEnableds = self.waitElements(expect, for: self.viewModel.isFinishInputEnabled) {
+            self.viewModel.toggleUpdateSelected("uid:3")
+            self.viewModel.toggleUpdateSelected("uid:3")
+        }
+
+        // then
+        XCTAssertEqual(isEnableds, [false, true, false])
+    }
+    
+    func testViewModel_refreshSuggestPlace() {
+        // given
+        let expect = expectation(description: "유저위치 기준 서제스트 새로고침")
+        expect.expectedFulfillmentCount = 2
+        self.stubDefaultList(0..<2)
+        self.initViewModel()
+        
+        // when
+        let cellViewModelLists = self.waitElements(expect, for: self.viewModel.cellViewModels) {
+            self.viewModel.refreshUserLocation()
+        }
+        
+        // then
+        XCTAssertEqual(cellViewModelLists.count, 2)
+    }
+}
+
+extension SelectHoorayPlaceViewModelTests {
+    
+    // 스킵시 알럿
+    func testViewModel_whenSkipPlaceInput_alert() {
+        // given
+        let expect = expectation(description: "장소정보 입력 스킵시도시에 알럿")
+        self.stubDefaultList(0..<10)
+        self.initViewModel()
+        
+        self.spyRouter.called(key: "alertForConfirm") { _ in
+            expect.fulfill()
+        }
+        
+        // when
+        self.viewModel.skipPlaceInput()
+        
+        // then
+        self.wait(for: [expect], timeout: self.timeout)
+    }
+    
+    // 새위치 추가 라우팅
+    func testViewModel_routeToRegisterNewPlace() {
+        // given
+        let expect = expectation(description: "장소정보 입력 스킵시도시에 알럿")
+        self.stubDefaultList(0..<10)
+        self.initViewModel()
+        
+        self.spyRouter.called(key: "presentNewPlaceRegisterScene") { _ in
+            expect.fulfill()
+        }
+        
+        // when
+        self.viewModel.registerNewPlace()
+        
+        // then
+        self.wait(for: [expect], timeout: self.timeout)
+    }
+    
+    // 새위치 추가 완료시 위치 표시하고 닫기
+}
+
+
+extension SelectHoorayPlaceViewModelTests {
+    
+    class SpyRouter: SelectHoorayPlaceRouting, Stubbable {
+        
+        func alertForConfirm(_ form: AlertForm) {
+            self.verify(key: "alertForConfirm")
+        }
+        
+        func presentNewPlaceRegisterScene() {
+            self.verify(key: "presentNewPlaceRegisterScene")
+        }
+    }
+}
