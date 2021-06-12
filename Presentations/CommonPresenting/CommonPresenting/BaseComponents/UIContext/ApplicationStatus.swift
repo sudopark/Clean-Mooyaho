a3d57dd1a74e485049092bceb1286f106dd7c005
sudo.park <sudo.park@kakao.com>
2021-06-13 05:56:03 +0900

    UIContext를 통하여 현재 앱 상태가 공유되도록 설정

---
+++ Presentations/CommonPresenting/CommonPresenting/BaseComponents/UIContext/ApplicationStatus.swift
@@ -0,0 +1,16 @@
+//
+//  ApplicationStatus.swift
+//  CommonPresenting
+//
+//  Created by sudo.park on 2021/06/13.
+//
+
+import Foundation
+
+public enum ApplicationStatus {
+    case idle
+    case launched
+    case forground
+    case background
+    case terminate
+}
