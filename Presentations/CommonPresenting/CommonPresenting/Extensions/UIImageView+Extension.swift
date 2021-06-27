c94f7f886679ae2c23e336466b27afb3b19e07c4
sudo.park <sudo.park@kakao.com>
2021-06-27 17:01:46 +0900

    이미지 렌더링시에 이미지뷰 사이즈에 맞추어 그리도록 수정

--- Presentations/CommonPresenting/CommonPresenting/Extensions/UIImageView+Extension.swift
+++ Presentations/CommonPresenting/CommonPresenting/Extensions/UIImageView+Extension.swift
@@ -12,8 +12,14 @@ import Kingfisher
 
 extension UIImageView {
     
+    private func resizeTargetSize(_ requestedSize: CGSize?) -> CGSize {
+        let scale = UIScreen.main.scale
+        let size = requestedSize ?? self.bounds.size
+        return CGSize(width: size.width * scale, height: size.height * scale)
+    }
     
     public func setupThumbnail(_ source: String,
+                               resize: CGSize? = nil,
                                progress: ((Int64, Int64) -> Void)? = nil,
                                completed: ((Result<UIImage, Error>) -> Void)? = nil) {
         guard let url = URL(string: source) else {
@@ -21,15 +27,18 @@ extension UIImageView {
             return
         }
         
+        let targetSize = self.resizeTargetSize(resize)
+        let processor = ResizingImageProcessor(referenceSize: targetSize)
+        
         if url.absoluteString.hasPrefix("file://") {
             let provider = LocalFileImageDataProvider(fileURL: url)
-            self.kf.setImage(with: provider) { result in
+            self.kf.setImage(with: provider, options: [.processor(processor)]) { result in
                 let mapResult = result.map{ $0.image }.mapError{ error -> Error in error }
                 completed?(mapResult)
             }
         } else {
             
-            self.kf.setImage(with: url, progressBlock: progress) { result in
+            self.kf.setImage(with: url, options: [.processor(processor)], progressBlock: progress) { result in
                 let mapResult = result.map{ $0.image }.mapError{ error -> Error in error }
                 completed?(mapResult)
             }
