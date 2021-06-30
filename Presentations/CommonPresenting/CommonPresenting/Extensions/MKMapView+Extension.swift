9f3d809a51dc7c81dee24941d6bec0a9cc8f1e48
sudo.park <sudo.park@kakao.com>
2021-07-01 02:23:36 +0900

    후레이 장소선택 지도 UI 구현

--- Presentations/CommonPresenting/CommonPresenting/Extensions/MKMapView+Extension.swift
+++ Presentations/CommonPresenting/CommonPresenting/Extensions/MKMapView+Extension.swift
@@ -37,3 +37,49 @@ extension MKMapView {
         self.setRegion(region, animated: animation)
     }
 }
+
+
+// AnnotationView
+
+public protocol AnnotationView: MKAnnotationView {
+    
+    associatedtype Annotation: NSObject & MKAnnotation
+    
+    func setup(for annotation: Annotation)
+}
+
+extension MKMapView {
+    
+    public func register<V: AnnotationView>(annotationView type: V.Type,
+                                            with customIdentifier: String? = nil) {
+        let identifier = customIdentifier ?? NSStringFromClass(V.Annotation.self)
+        self.register(V.self, forAnnotationViewWithReuseIdentifier: identifier)
+    }
+    
+    public func registerMarkerAnnotationView<A: MKAnnotation>(for type: A.Type,
+                                                              with customIdentifier: String? = nil) {
+        let identifier = customIdentifier ?? NSStringFromClass(A.self)
+        self.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: identifier)
+    }
+    
+    public func dequeue<V: AnnotationView>(for annotation: V.Annotation,
+                                           with customIdentifier: String? = nil) -> V {
+        let identifier = customIdentifier ?? NSStringFromClass(V.Annotation.self)
+        return self.dequeueReusableAnnotationView(withIdentifier: identifier, for: annotation) as! V
+    }
+    
+    public func deqeueMarketAnnotationView<A: MKAnnotation>(for annotation: A,
+                                                            with customIdentifier: String? = nil) -> MKMarkerAnnotationView {
+        let identifier = customIdentifier ?? NSStringFromClass(A.self)
+        return self.dequeueReusableAnnotationView(withIdentifier: identifier, for: annotation.self) as! MKMarkerAnnotationView
+    }
+}
+
+
+extension MKAnnotation {
+    
+    public var isUserLocation: Bool {
+        
+        return self.isKind(of: MKUserLocation.self)
+    }
+}
