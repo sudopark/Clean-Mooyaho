d69e6dc461b04584857049858012592c3531e521
sudo.park <sudo.park@kakao.com>
2021-08-20 04:32:11 +0900

    hooray marker로 줌 인아웃 및 overlay 추가

--- Domain/Domain/Policy.swift
+++ Domain/Domain/Policy.swift
@@ -12,7 +12,7 @@ public enum Policy {
     
     public static let defaultCooltime: TimeInterval = 10 * 60
     
-    public static let hoorayMaxSpreadDistance: Meters = 5_1000
+    public static let hoorayMaxSpreadDistance: Meters = 5_000
     public static let hoorayDefaultSpreadDistance: Meters = 1_000
     public static let hoorayPublishRangeDistance: Meters = 150
     
---
+++ Presentations/LocationScenes/MapScenes/Nearby/HoorayMarkerAnnotation+View.swift
@@ -0,0 +1,131 @@
+//
+//  HoorayMarkerAnnotation+View.swift
+//  MapScenes
+//
+//  Created by sudo.park on 2021/08/20.
+//
+
+import UIKit
+import MapKit
+
+import RxSwift
+import RxCocoa
+
+import Domain
+import CommonPresenting
+
+
+// MARK: - HoorayMarkerAnnotation
+
+final class HoorayMarkerAnnotation: NSObject, MKAnnotation {
+    
+    let marker: HoorayMarker
+    @objc var coordinate: CLLocationCoordinate2D
+    
+    init(marker: HoorayMarker) {
+        self.marker = marker
+        self.coordinate = .init(latitude: marker.coordinate.latt, longitude: marker.coordinate.long)
+    }
+}
+
+
+// MARK: - HoorayMarkerAnnotationView
+
+final class HoorayMarkerAnnotationView: MKAnnotationView, AnnotationView, Presenting {
+    
+    typealias Annotation = HoorayMarkerAnnotation
+    
+    private let backgroundView = UIVisualEffectView(effect: UIBlurEffect(style: .systemThickMaterial))
+    private let publisherImageView = IntegratedImageView()
+    
+    private var disposeBag = DisposeBag()
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        self.setupLayout()
+        self.setupStyling()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.disposeBag = DisposeBag()
+        self.publisherImageView.cancelSetupImage()
+    }
+    
+    func setup(for annotation: HoorayMarkerAnnotation) { }
+    
+    func bindUserInfo(_ source: Observable<Member>) {
+        
+        source
+            .distinctUntilChanged{ $0.icon == $1.icon }
+            .asDriver(onErrorDriveWith: .never())
+            .drive(onNext: { [weak self] member in
+                let icon = member.icon ?? Member.memberDefaultEmoji
+                self?.publisherImageView
+                    .setupImage(using: icon, resize: .init(width: 25, height: 25))
+            })
+            .disposed(by: self.disposeBag)
+    }
+}
+
+
+extension HoorayMarkerAnnotationView {
+    
+    func setupLayout() {
+        
+        self.frame = .init(x: 0, y: 0, width: 30, height: 30)
+        
+        self.addSubview(backgroundView)
+        backgroundView.autoLayout.fill(self)
+        
+        self.backgroundView.contentView.addSubview(publisherImageView)
+        publisherImageView.autoLayout.fill(self.backgroundView,
+                                           edges: .init(top: 2.5, left: 2.5,
+                                                        bottom: 2.5, right: 2.5),
+                                           withSafeArea: false)
+        publisherImageView.autoLayout.active {
+            $0.widthAnchor.constraint(equalToConstant: 25)
+            $0.heightAnchor.constraint(equalToConstant: 25)
+        }
+        publisherImageView.setupLayout()
+    }
+    
+    func setupStyling() {
+        
+        self.backgroundView.clipsToBounds = true
+        self.backgroundView.layer.cornerRadius = 15
+        
+        self.publisherImageView.setupStyling()
+    }
+}
+
+
+// MARK: - SpreadingOverlayCircle
+
+final class SpreadingOverlayCircle: MKCircle {
+    
+    let uuid: String = UUID().uuidString
+}
+
+//
+//final class SpreadingOverlayRenderer: MKOverlayRenderer {
+//    
+//    private var spreadingView: UIView?
+//    
+//    func startSpreading() {
+//        self.stopSpreading()
+//        
+//        let view = UIView()
+//        view.backgroundColor = .red
+//        
+//        
+//    }
+//    
+//    func stopSpreading() {
+//        
+//    }
+//}
