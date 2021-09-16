0ef23fe637ccbecbec2ec53291d7d8b107aa72aa
sudo.park <sudo.park@kakao.com>
2021-09-16 23:06:09 +0900

    ReadItem & Collection 저장 추가

--- Domain/Domain/Entities/LeadingItems/ReadLink.swift
+++ Domain/Domain/Entities/LeadingItems/ReadLink.swift
@@ -14,7 +14,7 @@ public struct ReadLink: ReadItem {
     private static let uidPrefix = "ri"
     
     public let uid: String
-    public let parentID: String?
+    public var parentID: String?
     public let link: String
     public let createdAt: TimeStamp
     public var lastUpdatedAt: TimeStamp
@@ -22,18 +22,16 @@ public struct ReadLink: ReadItem {
     public var priority: ReadPriority?
     public var categories: [Category] = []
     
-    public init(uid: String, parentID: String?,
-                link: String, createAt: TimeStamp, lastUpdated: TimeStamp) {
+    public init(uid: String, link: String,
+                createAt: TimeStamp, lastUpdated: TimeStamp) {
         self.uid = uid
-        self.parentID = parentID
         self.link = link
         self.createdAt = createAt
         self.lastUpdatedAt = lastUpdated
     }
     
-    public init(parentID: String?, link: String) {
+    public init(link: String) {
         self.uid = "\(Self.uidPrefix):\(UUID().uuidString)"
-        self.parentID = parentID
         self.link = link
         self.createdAt = TimeStamp.now()
         self.lastUpdatedAt = TimeStamp.now()
--- Domain/Domain/Usecases/Read/ReadItemUpdateUsecase.swift
+++ Domain/Domain/Usecases/Read/ReadItemUpdateUsecase.swift
@@ -9,16 +9,28 @@
 import Foundation
 
 import RxSwift
+import Prelude
+import Optics
 
 
 public protocol ReadItemUpdateUsecase {
     
-    func makeCollection(_ collection: ReadCollection,
-                        at parentID: String?) -> Maybe<Void>
-    
     func updateCollection(_ newCollection: ReadCollection) -> Maybe<Void>
     
-    func saveLink(_ link: String, at collectionID: String?) -> Maybe<Void>
-    
     func saveLink(_ link: ReadLink, at collectionID: String?) -> Maybe<Void>
+}
+
+
+extension ReadItemUpdateUsecase {
+    
+    public func makeCollection(_ collection: ReadCollection,
+                        at parentID: String?) -> Maybe<Void> {
+        let newCollection = collection |> \.parentID .~ parentID
+        return self.updateCollection(newCollection)
+    }
+    
+    public func saveLink(_ link: String, at collectionID: String?) -> Maybe<Void> {
+        let readLink = ReadLink(link: link)
+        return self.saveLink(readLink, at: collectionID)
+    }
 }
