a87a7deecd12e2f72ed665e03323beb3e3d93c89
sudo.park <sudo.park@kakao.com>
2021-09-22 13:34:38 +0900

    WSTagsField 삭제

--- Presentations/CommonPresenting/CommonPresenting/CustomViews/TextTagInputField.swift
+++ Presentations/CommonPresenting/CommonPresenting/CustomViews/TextTagInputField.swift
@@ -9,7 +9,6 @@ import UIKit
 
 import RxSwift
 import RxCocoa
-import WSTagsField
 
 // MARK: - Tag
 
@@ -25,24 +24,24 @@ public struct TextTag {
 }
 
 
-final class UUIDTagInputField: WSTagsField {
-    
-    override func addTag(_ tag: WSTag) {
-        let context = tag.context ?? UUID().uuidString as AnyHashable
-        let tagElement = WSTag(tag.text, context: context)
-        super.addTag(tagElement)
-    }
-    
-    override func addTag(_ tag: String) {
-        let tagElement = WSTag(tag, context: UUID().uuidString)
-        super.addTag(tagElement)
-    }
-    
-    
-    override func becomeFirstResponder() -> Bool {
-        return self.textField.becomeFirstResponder()
-    }
-}
+//final class UUIDTagInputField: WSTagsField {
+//
+//    override func addTag(_ tag: WSTag) {
+//        let context = tag.context ?? UUID().uuidString as AnyHashable
+//        let tagElement = WSTag(tag.text, context: context)
+//        super.addTag(tagElement)
+//    }
+//
+//    override func addTag(_ tag: String) {
+//        let tagElement = WSTag(tag, context: UUID().uuidString)
+//        super.addTag(tagElement)
+//    }
+//
+//
+//    override func becomeFirstResponder() -> Bool {
+//        return self.textField.becomeFirstResponder()
+//    }
+//}
 
 public final class TextTagInputField: BaseUIView {
     
@@ -54,13 +53,13 @@ public final class TextTagInputField: BaseUIView {
     
     public var isEnabled: Bool {
         get {
-            return self.underlyingTextField.textField.isEnabled
+            return self.underlyingTextField.isEnabled
         } set {
-            self.underlyingTextField.textField.isEnabled = newValue
+            self.underlyingTextField.isEnabled = newValue
         }
     }
     
-    private let underlyingTextField: UUIDTagInputField = .init()
+    private let underlyingTextField: UITextField = .init()
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
@@ -75,7 +74,7 @@ public final class TextTagInputField: BaseUIView {
     }
     
     public func appendTags(_ tagTexts: [String]) {
-        self.underlyingTextField.addTags(tagTexts)
+        
     }
 }
 
@@ -90,8 +89,8 @@ extension TextTagInputField: Presenting {
     
     public func setupStyling() {
         
-        self.underlyingTextField.textField.returnKeyType = .default
-        self.underlyingTextField.acceptTagOption = .space
+//        self.underlyingTextField.textField.returnKeyType = .default
+//        self.underlyingTextField.acceptTagOption = .space
         self.underlyingTextField.layoutMargins = UIEdgeInsets(top: 2, left: 1, bottom: 2, right: 1)
         self.underlyingTextField.tintColor = .systemBlue.withAlphaComponent(0.95)
     }
@@ -101,34 +100,15 @@ extension TextTagInputField: Presenting {
 extension TextTagInputField {
     
     public var didAppendTag: Observable<TextTag> {
-        return Observable.create { [weak self] observer in
-            guard let self = self else { return Disposables.create() }
-            self.underlyingTextField.onDidAddTag = { _, tag in
-                guard let uuid = tag.context as? String else { return }
-                let tag = TextTag(customIdentifier: uuid, text: tag.text)
-                observer.onNext(tag)
-            }
-            return Disposables.create()
-        }
+        return .empty()
+        
     }
     
     public var didRemoveTag: Observable<TextTag> {
-        return Observable.create { [weak self] observer in
-            guard let self = self else { return Disposables.create() }
-            self.underlyingTextField.onDidRemoveTag = { _, tag in
-                guard let uuid = tag.context as? String else { return }
-                let tag = TextTag(customIdentifier: uuid, text: tag.text)
-                observer.onNext(tag)
-            }
-            return Disposables.create()
-        }
+        return .empty()
     }
     
     public func getAllTags() -> [TextTag] {
-        return self.underlyingTextField.tags
-            .compactMap { wsTag -> TextTag? in
-                guard let uuid = wsTag.context as? String else { return nil }
-                return TextTag(customIdentifier: uuid, text: wsTag.text)
-            }
+        return []
     }
 }
