f2ed3296af452f1265a17ed98ed8f144e6bafbab
sudo.park <sudo.park@kakao.com>
2021-10-04 20:45:20 +0900

    ViewerScene framework 추가

---
+++ Presentations/ViewerScene/ViewerSceneTests/ViewerSceneTests.swift
@@ -0,0 +1,20 @@
+//
+//  ViewerSceneTests.swift
+//  ViewerSceneTests
+//
+//  Created by sudo.park on 2021/10/04.
+//
+
+import XCTest
+@testable import ViewerScene
+
+class ViewerSceneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
