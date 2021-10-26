ec0846f3cdba144115792a768942305c46f17af2
sudo.park <sudo.park@kakao.com>
2021-10-27 00:06:04 +0900

    [#300] 테스트 타겟 추가 및  인증서 세팅

---
+++ MooyahoApp/ReadReminderShareExtensionTests/ReadReminderShareExtensionTests.swift
@@ -0,0 +1,28 @@
+//
+//  ReadReminderShareExtensionTests.swift
+//  ReadReminderShareExtensionTests
+//
+//  Created by sudo.park on 2021/10/26.
+//  Copyright © 2021 ParkHyunsoo. All rights reserved.
+//
+
+import XCTest
+
+
+
+class ReadReminderShareExtensionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
