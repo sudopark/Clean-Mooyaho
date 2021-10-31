26dff80e7a17260eb61e015f343113ee172662d9
sudo.park <sudo.park@kakao.com>
2021-10-31 14:35:42 +0900

    [#313] 메인뷰 추가버튼 쉐도우 방식 변경

--- Presentations/CommonPresenting/CommonPresenting/CustomViews/RoundShadowView.swift
+++ Presentations/CommonPresenting/CommonPresenting/CustomViews/RoundShadowView.swift
@@ -13,6 +13,8 @@ public class RoundShadowView: BaseUIView {
     private var shadowLayer: CAShapeLayer!
     public var cornerRadius: CGFloat = 15.0
     public var fillColor: UIColor = .white
+    public var shadowOpacity: Float = 0.4
+    public var shadowColor: UIColor = UIColor.black.withAlphaComponent(0.4)
     
     public func updateLayer() {
         self.shadowLayer = nil
@@ -25,10 +27,10 @@ public class RoundShadowView: BaseUIView {
         let newLayer = CAShapeLayer()
         newLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: self.cornerRadius).cgPath
         newLayer.fillColor = self.fillColor.cgColor
-        newLayer.shadowColor = UIColor.black.withAlphaComponent(0.4).cgColor
+        newLayer.shadowColor = shadowColor.cgColor
         newLayer.shadowPath = newLayer.path
         newLayer.shadowOffset = .init(width: 0, height: 0.1)
-        newLayer.shadowOpacity = 0.4
+        newLayer.shadowOpacity = self.shadowOpacity
         newLayer.shadowRadius = self.cornerRadius
         
         self.shadowLayer = newLayer
