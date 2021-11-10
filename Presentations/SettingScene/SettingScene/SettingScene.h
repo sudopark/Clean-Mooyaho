65f958c6233134e0c52b0e22bf3a8d6079618c46
sudo.park <sudo.park@kakao.com>
2021-11-11 06:38:55 +0900

    [#347] Setting framework 추가

---
+++ Presentations/SettingScene/SettingScene/SettingScene.h
@@ -0,0 +1,18 @@
+//
+//  SettingScene.h
+//  SettingScene
+//
+//  Created by sudo.park on 2021/11/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SettingScene.
+FOUNDATION_EXPORT double SettingSceneVersionNumber;
+
+//! Project version string for SettingScene.
+FOUNDATION_EXPORT const unsigned char SettingSceneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SettingScene/PublicHeader.h>
+
+
