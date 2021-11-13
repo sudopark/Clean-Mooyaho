4d4209f92b282e1eec15c2c8068e8c15d940ed4c
sudo.park <sudo.park@kakao.com>
2021-11-13 15:18:54 +0900

    [#355] discoveryScene framework 추가

---
+++ Presentations/Discovery/DiscoveryTests/DiscoveryTests.swift
@@ -0,0 +1,24 @@
+//
+//  DiscoveryTests.swift
+//  DiscoveryTests
+//
+//  Created by sudo.park on 2021/11/13.
+//
+
+import XCTest
+@testable import DiscoveryScene
+
+class DiscoveryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_DO() {
+        
+    }
+}
