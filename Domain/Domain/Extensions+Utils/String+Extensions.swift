48d3c827651c09142d420ebb21e1f266a22c6d57
sudo.park <sudo.park@kakao.com>
2021-11-21 16:19:54 +0900

    [#385] SearchableQueryTokenStoreService 추가

--- Domain/Domain/Extensions+Utils/String+Extensions.swift
+++ Domain/Domain/Extensions+Utils/String+Extensions.swift
@@ -30,3 +30,52 @@ extension String {
         return String(format: format, args)
     }
 }
+
+
+// Levenshtein edit distance calculator(https://gist.github.com/bgreenlee/52d93a1d8fa1b8c1f38b)
+
+extension String {
+    
+    private static func min(_ numbers: Int...) -> Int {
+        return numbers.reduce(numbers[0], {$0 < $1 ? $0 : $1})
+    }
+    
+    public static func levenshtein(aStr: String, bStr: String) -> Int {
+        // create character arrays
+        let a = Array(aStr)
+        let b = Array(bStr)
+
+        // initialize matrix of size |a|+1 * |b|+1 to zero
+        var dist = [[Int]]()
+        for _ in 0...a.count {
+            dist += [Array(repeating: 0, count: b.count + 1)]
+        }
+
+        // 'a' prefixes can be transformed into empty string by deleting every char
+        for i in 1...a.count {
+            dist[i][0] = i
+        }
+
+        // 'b' prefixes can be created from empty string by inserting every char
+        for j in 1...b.count {
+            dist[0][j] = j
+        }
+
+        for i in 1...a.count {
+            for j in 1...b.count {
+                if a[i-1] == b[j-1] {
+                    dist[i][j] = dist[i-1][j-1]  // noop
+                } else {
+                    dist[i][j] = String.min(
+                        dist[i-1][j] + 1,  // deletion
+                        dist[i][j-1] + 1,  // insertion
+                        dist[i-1][j-1] + 1  // substitution
+                    )
+                }
+            }
+        }
+
+        return dist[a.count][b.count]
+    }
+
+}
