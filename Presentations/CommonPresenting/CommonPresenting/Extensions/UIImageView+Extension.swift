472ba89be75ffe3926ff553cd31096ced9e06671
sudo.park <sudo.park@kakao.com>
2022-02-15 02:14:56 +0900

    [#555] swiftUI 전용 IntegratedImageView 구현
    - 지정된 사이즈로만 다운샘플링 가능 -> 디폴트값으로 이미지뷰 사이즈로 다운샘플링하는 옵션은 제거

--- Presentations/CommonPresenting/CommonPresenting/Extensions/UIImageView+Extension.swift
+++ Presentations/CommonPresenting/CommonPresenting/Extensions/UIImageView+Extension.swift
@@ -64,3 +64,98 @@ extension UIImageView {
         self.kf.setImage(with: nilProvider)
     }
 }
+
+
+// MARK: SwiftUI -  RemoteImage
+
+import SwiftUI
+
+extension Views {
+    
+    
+    public struct RemoteImage: View {
+        
+        @Binding var imageSource: ImageSource
+        private let resize: CGSize?
+        
+        public init(_ imageSource: Binding<ImageSource>, resize: CGSize? = nil) {
+            self._imageSource = imageSource
+            self.resize = resize
+        }
+        
+        public var configurations: [(KFImage) -> KFImage] = []
+        
+        public var body: some View {
+            Group {
+                self.configurations
+                    .reduce(
+                        self.initialImage()
+                    ) { current, configue in
+                        configue(current)
+                    }
+                    .downSamplingIfNeed(self.resize)
+                    .cancelOnDisappear(true)
+                    .cacheOriginalImage()
+            }
+        }
+        
+        private func initialImage() -> KFImage {
+            if self.imageSource.path.hasPrefix("file://"),
+               let fileURL = URL(string: self.imageSource.path) {
+                let provider = LocalFileImageDataProvider(fileURL: fileURL)
+                return KFImage(source: .provider(provider))
+            } else {
+                return KFImage(self.imageSource.path)
+            }
+        }
+    }
+}
+
+extension Views.RemoteImage {
+    
+    public func resize(capInsets: EdgeInsets = EdgeInsets(),
+                       resizingMode: Image.ResizingMode = .stretch) -> Views.RemoteImage {
+        
+        let block: (KFImage) -> KFImage = { $0.resizable(capInsets: capInsets, resizingMode: resizingMode) }
+        return self.configue(with: block)
+    }
+    
+    public func renderingMode(_ renderingMode: Image.TemplateRenderingMode) -> Views.RemoteImage {
+        let block: (KFImage) -> KFImage = { $0.renderingMode(renderingMode) }
+        return self.configue(with: block)
+    }
+    
+    public func interpolation(_ interpolation: Image.Interpolation) -> Views.RemoteImage {
+        let block: (KFImage) -> KFImage = { $0.interpolation(interpolation) }
+        return self.configue(with: block)
+    }
+    
+    public func antialiased(_ isAntialiased: Bool) -> Views.RemoteImage {
+        let block: (KFImage) -> KFImage = { $0.antialiased(isAntialiased) }
+        return self.configue(with: block)
+    }
+    
+    private func configue(with block: @escaping (KFImage) -> KFImage) -> Views.RemoteImage {
+        var sender = self
+        sender.configurations = self.configurations + [block]
+        return sender
+    }
+}
+
+
+private extension KFImage {
+    
+    init(_ path: String) {
+        self.init(URL(string: path))
+    }
+    
+    func downSamplingIfNeed(_ resize: CGSize?) -> KFImage {
+        guard let resize = resize else { return self }
+        let scale = UIScreen.main.scale
+        return self
+            .downsampling(
+                size: .init(width: resize.width * scale,
+                            height: resize.height * scale)
+            )
+    }
+}
