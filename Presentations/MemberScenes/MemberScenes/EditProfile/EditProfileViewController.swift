5091eb15deed95efc5cbd37827c8828a11f51dfe
sudo.park <sudo.park@kakao.com>
2022-02-17 00:37:04 +0900

    [#555] 프로필 편집화면 swiftui로 변환

--- Presentations/MemberScenes/MemberScenes/EditProfile/EditProfileViewController.swift
+++ Presentations/MemberScenes/MemberScenes/EditProfile/EditProfileViewController.swift
@@ -1,16 +1,15 @@
 //
+//  
 //  EditProfileViewController.swift
 //  MemberScenes
 //
-//  Created sudo.park on 2021/05/30.
-//  Copyright © 2021 ___ORGANIZATIONNAME___. All rights reserved.
+//  Created by sudo.park on 2022/02/16.
+//
 //
 
-import UIKit
 
-import RxSwift
-import RxCocoa
-import RxDataSources
+import UIKit
+import SwiftUI
 
 import Domain
 import CommonPresenting
@@ -18,19 +17,15 @@ import CommonPresenting
 
 // MARK: - EditProfileViewController
 
-public final class EditProfileViewController: BaseViewController, EditProfileScene {
-    
-    typealias Sections = SectionModel<String, EditProfileCellViewModel>
-    typealias DataSource = RxTableViewSectionedReloadDataSource<Sections>
-    
-    let editView = EditProfileView()
+public final class EditProfileViewController: UIHostingController<EditProfileView>, EditProfileScene, BaseViewControllable {
     
     let viewModel: EditProfileViewModel
-    private var dataSource: DataSource!
     
     public init(viewModel: EditProfileViewModel) {
         self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
+        
+        let rootView = EditProfileView(viewModel: viewModel)
+        super.init(rootView: rootView)
     }
     
     required init?(coder: NSCoder) {
@@ -41,127 +36,8 @@ public final class EditProfileViewController: BaseViewController, EditProfileSce
         LeakDetector.instance.expectDeallocate(object: self.viewModel)
     }
     
-    public override func loadView() {
-        super.loadView()
-        self.setupLayout()
-        self.setupStyling()
-    }
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-        self.bind()
-    }
-    
-}
-
-// MARK: - bind
-
-extension EditProfileViewController {
-    
-    private func bind() {
-        
-        self.rx.viewDidLayoutSubviews.take(1)
-            .subscribe(onNext: { [weak self] _ in
-                self?.bindTableView()
-                self?.bindThumbnail()
-            })
-            .disposed(by: self.disposeBag)
-        
-        self.viewModel.isSavable
-            .asDriver(onErrorDriveWith: .never())
-            .drive(self.editView.saveButton.rx.isEnabled)
-            .disposed(by: self.disposeBag)
-        
-        // TODO: show is Saving
-        
-        self.editView.closeButton.rx.tap
-            .subscribe(onNext: { [weak self] in
-                self?.viewModel.requestCloseScene()
-            })
-            .disposed(by: self.disposeBag)
-        
-        self.editView.saveButton.rx.tap
-            .subscribe(onNext: { [weak self] in
-                self?.viewModel.saveChanges()
-            })
-            .disposed(by: self.disposeBag)
-    }
-}
-
-// MARK: - setup presenting
-
-extension EditProfileViewController: Presenting {
-    
-    
-    public func setupLayout() {
-        
-        self.view.addSubview(editView)
-        editView.autoLayout.fill(self.view)
-        editView.setupLayout()
-    }
-    
-    public func setupStyling() {
-        
-        self.view.backgroundColor = self.uiContext.colors.appBackground
-        
-        editView.setupStyling()
-    }
-}
-
-
-// MARK: - bind tableview
-
-private extension EditProfileViewController {
-    
-    func makeDataSource() -> DataSource {
-        
-        return .init { _, tableView, indexPath, cellViewModel in
-            let cell: EditProfileView.InputTextCell = tableView.dequeueCell()
-            cell.setupCell(cellViewModel)
-            return cell
-        }
-    }
-    
-    func bindTableView() {
-        
-        self.dataSource = self.makeDataSource()
-        
-        self.viewModel.cellViewModels
-            .map{ [Sections(model: "inputs", items: $0)] }
-            .asDriver(onErrorDriveWith: .never())
-            .drive(self.editView.tableView.rx.items(dataSource: self.dataSource))
-            .disposed(by: self.disposeBag)
-        
-        self.editView.tableView.rx.modelSelected(EditProfileCellViewModel.self)
-            .subscribe(onNext: { [weak self] cellViewModel in
-                self?.viewModel.requestChangeProperty(cellViewModel.inputType)
-            })
-            .disposed(by: self.disposeBag)
-    }
-}
-
-
-// MARK: - bind emoji, memoji input
-
-private extension EditProfileViewController {
-    
-    func bindThumbnail() {
-        
-        self.viewModel.profileImageSource
-            .asDriver(onErrorDriveWith: .never())
-            .drive(onNext: { [weak self] source in
-                if let source = source {
-                    self?.editView.profileHeaderView.imageView.setupImage(using: source)
-                } else {
-                    self?.editView.profileHeaderView.imageView.cancelSetupImage()
-                }
-            })
-            .disposed(by: self.disposeBag)
-        
-        self.editView.profileHeaderView.imageView.rx.addTapgestureRecognizer()
-            .subscribe(onNext: { [weak self] _ in
-                self?.viewModel.requestChangeThumbnail()
-            })
-            .disposed(by: self.disposeBag)
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
     }
 }
--- Presentations/MemberScenes/MemberScenes/EditProfile/EditProfileViewModel.swift
+++ Presentations/MemberScenes/MemberScenes/EditProfile/EditProfileViewModel.swift
@@ -20,7 +20,7 @@ import CommonPresenting
 
 public struct EditProfileCellViewModel: Equatable {
     
-    public enum InputType {
+    public enum InputType: String {
         case nickname
         case intro
     }
