a6d5fe663b8d7b0d88f07a8e45505fe9f94b3fc4
sudo.park <sudo.park@kakao.com>
2022-07-03 00:24:46 +0900

    [#629] SQLiteStorage 추가

---
+++ Repositories/ReadingList/ReadingListTests/LocalReadingListRepositoryTests.swift
@@ -0,0 +1,18 @@
+//
+//  LocalReadingListRepositoryTests.swift
+//  ReadingListTests
+//
+//  Created by sudo.park on 2022/07/09.
+//
+
+import XCTest
+
+import UnitTestHelpKit
+
+
+class LocalReadingListRepositoryTests: BaseTestCase {
+    
+    func test_dummy() {
+        XCTAssert(true)
+    }
+}
