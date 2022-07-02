5143d11c7552196e4c452f7d6aedf290734ef620
sudo.park <sudo.park@kakao.com>
2022-07-02 14:44:20 +0900

    [#629] Local framework 추가

---
+++ Supports/Local/Local/Local.h
@@ -0,0 +1,18 @@
+//
+//  Local.h
+//  Local
+//
+//  Created by sudo.park on 2022/07/02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Local.
+FOUNDATION_EXPORT double LocalVersionNumber;
+
+//! Project version string for Local.
+FOUNDATION_EXPORT const unsigned char LocalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Local/PublicHeader.h>
+
+
