4b62beeac02ca5e8302c94d107ab5ee3422fda6d
sudo.park <sudo.park@kakao.com>
2022-07-12 00:37:02 +0900

    [#633] Remote framework 추가

---
+++ Supports/Remote/Remote/Remote.h
@@ -0,0 +1,18 @@
+//
+//  Remote.h
+//  Remote
+//
+//  Created by sudo.park on 2022/07/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Remote.
+FOUNDATION_EXPORT double RemoteVersionNumber;
+
+//! Project version string for Remote.
+FOUNDATION_EXPORT const unsigned char RemoteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Remote/PublicHeader.h>
+
+
---
+++ Supports/Remote/RemoteTests/RemoteTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteTests.swift
+//  RemoteTests
+//
+//  Created by sudo.park on 2022/07/12.
+//
+
+import XCTest
+@testable import Remote
+
+class RemoteTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
