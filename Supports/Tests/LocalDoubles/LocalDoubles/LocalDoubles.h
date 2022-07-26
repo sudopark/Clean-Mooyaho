c33f839aa742769f26d8b6fecaf02d1c933ab7c1
sudo.park <sudo.park@kakao.com>
2022-07-27 02:40:44 +0900

    [#620] LocalDoubles 추가
    - MockSecureStorage 추가
    - Environment, SQL storage는 실제 저장소 사용하도록 세팅

---
+++ Supports/Tests/LocalDoubles/LocalDoubles/LocalDoubles.h
@@ -0,0 +1,18 @@
+//
+//  LocalDoubles.h
+//  LocalDoubles
+//
+//  Created by sudo.park on 2022/07/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalDoubles.
+FOUNDATION_EXPORT double LocalDoublesVersionNumber;
+
+//! Project version string for LocalDoubles.
+FOUNDATION_EXPORT const unsigned char LocalDoublesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalDoubles/PublicHeader.h>
+
+
