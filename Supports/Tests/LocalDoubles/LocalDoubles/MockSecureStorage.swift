237df4d9598dac5ea15a9a626f8d705cef403927
sudo.park <sudo.park@kakao.com>
2022-07-30 14:09:14 +0900

    [#620] RemoteDoubles 추가

--- Supports/Tests/LocalDoubles/LocalDoubles/MockSecureStorage.swift
+++ Supports/Tests/LocalDoubles/LocalDoubles/MockSecureStorage.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 import Local
+import Extensions
 
 open class MockSecureStorage: SecureStorage {
     
@@ -29,28 +30,5 @@ open class MockSecureStorage: SecureStorage {
     public var removeResult: Result<Void, Error>?
     open func remove(_ key: String) throws {
         try self.removeResult?.throwOrNot()
-    }
-}
-
-
-private extension Result where Success == Void {
-    
-    func throwOrNot() throws {
-        switch self {
-        case .success: return
-        case .failure(let error): throw error
-        }
-    }
-}
-
-
-private extension Result where Success == Any {
-    
-    func unwrapSuccessOrThrow<T>() throws -> T? {
-        let resultWithType = self.map { $0 as? T }
-        switch resultWithType {
-        case .success(let t): return t
-        case .failure(let error): throw error
-        }
     }
 }
---
+++ Supports/Tests/RemoteDoubles/RemoteDoubles/RemoteDoubles/RemoteDoubles.h
@@ -0,0 +1,18 @@
+//
+//  RemoteDoubles.h
+//  RemoteDoubles
+//
+//  Created by sudo.park on 2022/07/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteDoubles.
+FOUNDATION_EXPORT double RemoteDoublesVersionNumber;
+
+//! Project version string for RemoteDoubles.
+FOUNDATION_EXPORT const unsigned char RemoteDoublesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteDoubles/PublicHeader.h>
+
+
