4dc18ec27a6e883812126ad5fc173a44dfb842e5
sudo.park <sudo.park@kakao.com>
2022-08-29 01:58:14 +0900

    [#645] Repository Usage - LoadWithCache 추가

---
+++ Domain/Domain/Repositories/v2/Repository+Usage/LoadWithCache.swift
@@ -0,0 +1,57 @@
+//
+//  LoadWithCache.swift
+//  Domain
+//
+//  Created by sudo.park on 2022/08/29.
+//  Copyright © 2022 ParkHyunsoo. All rights reserved.
+//
+
+import Foundation
+
+
+public struct LoadWithCache<Local: Sendable, Remote: Sendable>: Sendable {
+    
+    private let authInfoProvider: AuthInfoProviderV2
+    private let local: Local
+    private let remote: Remote
+    
+    public init(
+        _ authInfoProvider: AuthInfoProviderV2,
+        _ local: Local,
+        _ remote: Remote
+    ) {
+        self.authInfoProvider = authInfoProvider
+        self.local = local
+        self.remote = remote
+    }
+    
+    public func `do`<T: Sendable>(
+        _ localFetching: @Sendable @escaping (Local) async throws -> [T],
+        thenLoadFromRemote: (@Sendable (Remote, String, [T]) async throws -> [T])? = nil,
+        andUpdateLocal: (@Sendable (Local, [T]) async throws -> Void)? = nil
+    ) async throws -> [T] {
+        
+        let loadFromLocal = try await localFetching(self.local)
+        let remoteData = try await loadFromRemoteIfNeed(localData: loadFromLocal, thenLoadFromRemote, andUpdateLocal)
+        return loadFromLocal + (remoteData ?? [])
+    }
+    
+    private func loadFromRemoteIfNeed<T: Sendable>(
+        localData: [T],
+        _ loadFromRemote: (@Sendable (Remote, String, [T]) async throws -> [T])?,
+        _ updateLocal: (@Sendable (Local, [T]) async throws -> Void)?
+    ) async throws -> [T]? {
+        
+        guard let remoteLoading = loadFromRemote,
+              let ownerID = await authInfoProvider.signInMemberID()
+        else {
+            return nil
+        }
+        
+        let remoteData = try await remoteLoading(self.remote, ownerID, localData)
+        if let refreshing = updateLocal, remoteData.isNotEmpty {
+            Task.detached { try await refreshing(self.local, remoteData) }
+        }
+        return remoteData
+    }
+}
