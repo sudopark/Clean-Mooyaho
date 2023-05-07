aa03253747d79a2c3520116dea29d9438a593a5f
sudo.park <sudo.park@kakao.com>
2023-05-07 16:47:54 +0900

    [#665] swiftUI 웹뷰 - 마지막 읽음위치로 이동 기능 구현

--- Presentations/CommonPresenting/CommonPresenting/CustomViews/BaseFloatingButton.swift
+++ Presentations/CommonPresenting/CommonPresenting/CustomViews/BaseFloatingButton.swift
@@ -125,3 +125,73 @@ public extension Reactive where Base: BaseFloatingButton {
             .map { _ in }
     }
 }
+
+
+// MARK: - swiftUI baseFloatingButton
+
+import SwiftUI
+
+extension Views {
+    
+    public struct BaseFloatingButton: View {
+        
+        @State public var title: String
+        @State public var description: String
+        
+        public init(title: String, description: String) {
+            self.title = title
+            self.description = description
+        }
+        
+        public var mainActionHandler: () -> Void = { }
+        public var closeActionHandler: () -> Void = { }
+        
+        public var body: some View {
+            HStack {
+                
+                Spacer()
+                
+                Button {
+                    self.mainActionHandler()
+                } label: {
+                    VStack(spacing: 5) {
+                        Text(self.title)
+                            .font(self.theme.fonts.get(15, weight: .medium).asFont)
+                            .foregroundColor(self.theme.colors.text.asColor)
+                        Text(self.description)
+                            .font(self.theme.fonts.get(12, weight: .regular).asFont)
+                            .foregroundColor(self.theme.colors.secondaryTitle.asColor)
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    self.closeActionHandler()
+                } label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(self.theme.colors.text.asColor)
+                }
+            }
+            .padding([.top, .bottom], 8)
+            .padding([.leading, .trailing], 12)
+            .background(
+                Views.RoundShadowView(
+                    customBackgroundColor: self.theme.colors.appSecondBackground.asColor,
+                    cornerRadidus: 15.0
+                )
+            )
+        }
+    }
+}
+
+class BaseFloatingButtonPreviewProvider: PreviewProvider {
+    
+ 
+    static var previews: Views.BaseFloatingButton {
+        return Views.BaseFloatingButton(title: "reading-option-ask-last-position".localized, description: "some")
+    }
+}
